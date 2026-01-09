0433e875a7f3d058690c4a81cc3adc8fa900b3ae
Dimka122 <kassindima1@gmail.com>
2026-01-09 10:13:33 +0200

    Настройка Tailwind

---
+++ src/tailwind.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#FFB17A',
+          100: '#FF9E59',
+          200: '#FF5F00',
+          300: '#FF3D00',
+        },
+        grey: {
+          100: '#F4F4F4',
+          200: '#B4B4B4',
+          300: '#7E7E7E',
+          400: '#474747',
+          500: '#31393C',
+        },
+        primary: {
+          100: '#078691',
+        },
+      },
+      borderRadius: {
+        xl: '16px',
+      },
+    },
+  },
+  plugins: [],
+};
